8f3e4fe55415389ba322faf1d23fe59145f42b55
ramashishiameyainnovex <ram.kumar@iameya.in>
2025-11-10 21:56:53 +0530

    navigation button added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p className="text-red-500">Hello</p>
+    <div className="flex gap-12 min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <a href="/zunaki/portfolio" className="text-white p-2 shadow-lg rounded-xl bg-black whitespace-nowrap border border-gray-100">Zunaki Portfolio</a>
+      <a href="/ritik/portfolio" className="text-white p-2 shadow-lg rounded-xl bg-black whitespace-nowrap border border-gray-100">Ritik Portfolio</a>
+      
     </div>
   );
 }
